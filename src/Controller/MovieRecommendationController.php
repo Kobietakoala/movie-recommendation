fcda2d0e510b91a1b5d223de2c539b9aefdc7111
Kobietakoala <kobietakoala@gmail.com>
2025-07-29 18:50:28 +0200

    Task#86c4rdhhv Add MovieRecommendationController

--- src/Controller/MovieRecommendationController.php
+++ src/Controller/MovieRecommendationController.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieRecommendationController extends AbstractController
 {
-//
-//    #[Route('/movie-recommendation')]
-//    public function index()
-//    : JsonResponse
-//
-//    {
-//        return new JsonResponse
-//
-//    }
+    #[Route('/random-movies', name: 'random_movies', methods: ['GET'])]
+    public function getRandomMovies(): JsonResponse
+    {
+        return new JsonResponse();
+    }
+    #[Route('/movies-with-w-even-length', name: 'movies_with_w_even_length', methods: ['GET'])]
+    public function getMoviesWithWEvenLength(): JsonResponse
+    {
+        return new JsonResponse();
+    }
+    #[Route('/multi-word-movies', name: 'multi_word_movies', methods: ['GET'])]
+    public function getMultiWordMovies(): JsonResponse
+    {
+        return new JsonResponse();
+    }
 }
