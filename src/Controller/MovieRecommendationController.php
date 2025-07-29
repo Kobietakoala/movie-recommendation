82a02a358501c65b631cdbacc3533ec38dcc3604
Kobietakoala <kobietakoala@gmail.com>
2025-07-29 13:56:19 +0200

    Remove .idea folder from tracking

---
+++ src/Controller/MovieRecommendationController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieRecommendationController extends AbstractController
+{
+//
+//    #[Route('/movie-recommendation')]
+//    public function index()
+//    : JsonResponse
+//
+//    {
+//        return new JsonResponse
+//
+//    }
+}
