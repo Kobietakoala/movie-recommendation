7daa4d548624661bdbce8d5b11f2e893f468a577
Kobietakoala <kobietakoala@gmail.com>
2025-07-30 06:36:23 +0200

    Task#86c4rdp05 Remove circular dependency between trait and service

--- src/Traits/ErrorMessagesTrait.php
+++ src/Traits/ErrorMessagesTrait.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
-use App\Service\RecommendationService;
-
 trait ErrorMessagesTrait
 {
+    /**
+     * @param string $key
+     * @return string
+     */
     protected function getErrorMessage(string $key): string
     {
         return match ($key) {
-            'movies_not_found' => 'Plik ' . RecommendationService::MOVIES_FILE_PATH . ' nie został znaleziony',
+            'movies_not_found' => 'Plik data/movies.php nie został znaleziony',
             'invalid_movies_format' => 'Nie znaleziono tablicy $movies w pliku movies.php',
+            'non_positive_movie_count' => 'Liczba filmów do pobrania musi być większa niż 0',
             'internal' => 'Wystąpił nieoczekiwany błąd.',
             'missing_param' => 'Brak wymaganego parametru: %param%',
             default => 'Nieznany błąd'
